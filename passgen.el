9fa60e236e1a3aac4d22b5dcf11e50ea4e60133d
Marcos Moritz <moritz@portaltelemedicina.com.br>
2020-04-17 23:54:36 -0300

    Allow passgen to use numeric params
    It allows the use of passgen--generate to define how many characters
    of each kind it will require for the generator

--- passgen.el
+++ passgen.el
@@ -1,4 +1,4 @@
-(defvar passgen--special-chars "@%%+\\/'!#$^?:.(){}[]~`-_.")
+(defvar passgen--special-chars "@%%+/!#$^?:.~`-_.")
 (defvar passgen--alpha-lower "abcdefghijklmnopqrstuvwxyz")
 (defvar passgen--alpha-upper "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 (defvar passgen--numeric "0123456789")
@@ -10,29 +10,65 @@
     (passgen--generate length t t t t)))
 
 (defun passgen--generate (length &optional alpha-lower alpha-upper numeric special-chars)
-  (let ((alpha-lower (if (boundp alpha-lower)
-                         alpha-lower
-                       t))
-        (alpha-upper (if (boundp alpha-upper)
-                         alpha-upper
-                       t))
-        (special-chars (if (boundp special-chars)
-                           special-chars
-                         t))
-        (numeric (if (boundp numeric)
-                     numeric
-                   t))
-        (string-pool "")
+  (if (< length (+ (if (numberp alpha-lower) alpha-lower 0)
+                   (if (numberp alpha-upper) alpha-upper 0)
+                   (if (numberp numeric) numeric 0)
+                   (if (numberp special-chars) special-chars 0)))
+      (error "The `length' must be greater or equal the sum of the rest of the parms"))
+  (let ((char-pool "")
         (result))
     (if alpha-lower
-        (setq string-pool (concat string-pool passgen--alpha-lower)))
+        (progn
+          (setq result (append result (passgen--get 'alpha-lower)))
+          (setq char-pool (concat char-pool passgen--alpha-lower))))
+
     (if alpha-upper
-        (setq string-pool (concat string-pool passgen--alpha-upper)))
+        (progn
+          (setq result (append result (passgen--get 'alpha-upper)))
+          (setq char-pool (concat char-pool passgen--alpha-upper))))
+
     (if numeric
-        (setq string-pool (concat string-pool passgen--numeric)))
+        (progn
+          (setq result (append result (passgen--get 'numeric)))
+          (setq char-pool (concat char-pool passgen--numeric))))
+
     (if special-chars
-        (setq string-pool (concat string-pool passgen--special-chars)))
-    (dotimes (i length)
-      (let ((index (% (abs (random)) (length string-pool))))
-        (setq result (append result `(,(substring string-pool index (1+ index)))))))
+        (progn
+          (setq result (append result (passgen--get 'special-chars)))
+          (setq char-pool (concat char-pool passgen--special-chars))))
+
+    (if (= 0 (length char-pool))
+        (setq char-pool passgen--alpha-lower))
+    (setq remainder (- length (length result)))
+    (dotimes (i remainder)
+      (let ((index (% (abs (random)) (length char-pool))))
+        (setq result (append result `(,(substring char-pool index (1+ index)))))))
+    (setq result (shuffle result))
     (kill-new (mapconcat 'concat result ""))))
+
+(defun passgen--get (symbol)
+  (let* ((result)
+         (symbol-value (symbol-value symbol))
+         (symbol-name (symbol-name symbol))
+         (symbol-charlist (symbol-value (intern (concat "passgen--" symbol-name)))))
+    (if (numberp symbol-value)
+        (dotimes (i symbol-value)
+          (let ((index (% (abs (random)) (length symbol-charlist))))
+            (setq result (append result `(,(substring symbol-charlist index (1+ index))))))))
+    result))
+
+;; From http://kitchingroup.cheme.cmu.edu/blog/2014/09/06/Randomize-a-list-in-Emacs/
+(defun swap (LIST el1 el2)
+  "in LIST swap indices EL1 and EL2 in place"
+  (let ((tmp (elt LIST el1)))
+    (setf (elt LIST el1) (elt LIST el2))
+    (setf (elt LIST el2) tmp)))
+
+
+(defun shuffle (LIST)
+  "Shuffle the elements in LIST.
+shuffling is done in place."
+  (loop for i in (reverse (number-sequence 1 (1- (length LIST))))
+        do (let ((j (random (+ i 1))))
+             (swap LIST i j)))
+  LIST)
