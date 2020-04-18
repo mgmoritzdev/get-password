87b4d5be39577109cdca573216acf5db4991801c
Marcos Moritz <moritz@portaltelemedicina.com.br>
2020-04-18 01:01:54 -0300

    change the default passgen to allow the optional argument length

--- passgen.el
+++ passgen.el
@@ -1,13 +1,29 @@
+;; use passgen to copy a new password to kill-ring
+;; it's also possible to use the internal function passgen--generate
+;; to select which kind of characters you want in your password and
+;; how many.
+;;
+;; Usage examples:
+;; basic:
+;; (passgen 16)
+;; => returns the password with all kinds of chars programmed
+;;
+;; using internal function passgen--generate
+;; (passgen--generate 16 10 1 1 1)
+;; returns the the password with at least 1 of each kind of chars
+
 (defvar passgen--special-chars "@%%+/!#$^?:.~`-_.")
 (defvar passgen--alpha-lower "abcdefghijklmnopqrstuvwxyz")
 (defvar passgen--alpha-upper "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 (defvar passgen--numeric "0123456789")
 
-(defun passgen ()
+(defun passgen (&optional arg)
   "Generates a strong password with `LENGTH` characters."
-  (interactive)
-  (let ((length (read-number "Type the password length: ")))
-    (passgen--generate length t t t t)))
+  (interactive "p")
+  (if (<= arg 1)
+      (let ((length (read-number "Type the password length: ")))
+        (passgen--generate length 1 1 1 1))
+    (passgen--generate arg 1 1 1 1)))
 
 (defun passgen--generate (length &optional alpha-lower alpha-upper numeric special-chars)
   (if (< length (+ (if (numberp alpha-lower) alpha-lower 0)
